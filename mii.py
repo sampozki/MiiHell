a72e64b6cafbaa54a24e416c3652ab85d06189fa
Sampo <pelto.sampo@gmail.com>
2018-08-21 22:01:15 +0300

    Better

--- mii.py
+++ mii.py
@@ -1,16 +1,16 @@
-import winsound
-import os
-import time
-import random
+from winsound import PlaySound, SND_FILENAME
+from os import listdir, path
+from time import sleep
+from random import uniform
 
 
 def main():
     directory = 'Mii/'
-    sounds = sorted(os.listdir(directory), key=lambda x: int(os.path.splitext(x)[0]))
+    soundlist = sorted(listdir(directory), key=lambda x: int(path.splitext(x)[0]))
     while True:
-        for sound in sounds:
-            winsound.PlaySound(directory + sound, winsound.SND_FILENAME)
-            time.sleep(round(random.uniform(0, 3), 1))
+        for sound in soundlist:
+            PlaySound(directory + sound, SND_FILENAME)
+            sleep(round(uniform(0, 3), 1))
 
 
 if __name__ == '__main__':
