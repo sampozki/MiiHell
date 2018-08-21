99e14649feaf4f4c9f131470b7a9b1cdfd83b8a1
Sampo <pelto.sampo@gmail.com>
2018-08-21 21:28:13 +0300

    Added randomizer

--- mii.py
+++ mii.py
@@ -1,1 +1,17 @@
+import winsound
+import os
+import random
 # TODO kaikki
+
+
+def main():
+    directory = 'Mii/'
+    sounds = os.listdir(directory)
+    while True:
+        sound = random.choice(sounds)
+        print(sound)
+        winsound.PlaySound(directory + sound, winsound.SND_FILENAME)
+
+
+if __name__ == '__main__':
+    main()
