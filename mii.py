6a59d40068029b741b842ac2d73a3fc87961112d
Sampo <pelto.sampo@gmail.com>
2018-08-22 13:16:25 +0300

    Added pattern mutation, loop counter and timer

--- mii.py
+++ mii.py
@@ -1,16 +1,33 @@
 from winsound import PlaySound, SND_FILENAME
 from os import listdir, path
-from time import sleep
-from random import uniform
+from time import sleep, time
+from random import uniform, randint
 
 
 def main():
+    start = time()
     directory = 'Mii/'
+    loop = 1
     soundlist = sorted(listdir(directory), key=lambda x: int(path.splitext(x)[0]))
+
+    print('Round: ' + str(loop) + '\nCurrent play order: ' + ', '.join(soundlist))
+
     while True:
         for sound in soundlist:
             PlaySound(directory + sound, SND_FILENAME)
             sleep(round(uniform(0, 3), 1))
+
+        for a in range(0, randint(1, 3)):
+            mutationa = randint(0, len(soundlist)-1)
+            mutationb = randint(0, len(soundlist)-1)
+            soundlist[mutationa], soundlist[mutationb] = soundlist[mutationb], soundlist[mutationa]
+            print(soundlist[mutationa] + " <-> " + soundlist[mutationb], end="  ")
+
+        loop += 1
+        playtime = round((time() - start) / 60, 1)
+
+        print('\n\nRound: ' + str(loop) + ' | Time Spend: ' + str(playtime) + ' minutes')
+        print('Current play order: ' + ', '.join(soundlist))
 
 
 if __name__ == '__main__':
