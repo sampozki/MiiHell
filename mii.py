ed58b3c31d38b97580fcb7e677e3243567c20f33
Sampo <pelto.sampo@gmail.com>
2018-08-24 00:00:27 +0300

    Added randomized pitch modulation

--- mii.py
+++ mii.py
@@ -3,28 +3,58 @@
 from winsound import PlaySound, SND_FILENAME
 from os import listdir, path
 from time import sleep, time
-from random import uniform, randint
+from random import uniform, randint, choice
+import shutil
+from pydub import AudioSegment
 
 
 def main():
     start = time()
-    directory = 'Mii/'
     loop = 1
-    soundlist = sorted(listdir(directory), key=lambda x: int(path.splitext(x)[0]))
+    destinationdirectory = 'Temp/'
+    directory = 'Mii/'
 
-    print('Round: ' + str(loop) + '\nCurrent play order: ' + ', '.join(soundlist))
+    if path.isdir(destinationdirectory):
+        shutil.rmtree(destinationdirectory)
+
+        shutil.copytree(directory, destinationdirectory)
+        print('Created new temporary folder from ' + directory + ' to ' + destinationdirectory)
+    else:
+        shutil.copytree(directory, destinationdirectory)
+        print('Created temporary folder from ' + directory + ' to ' + destinationdirectory)
+
+    soundlist = sorted(listdir(destinationdirectory), key=lambda x: int(path.splitext(x)[0]))
+
+    print('Original play order: ' + ', '.join(soundlist))
 
     while True:
         for sound in soundlist:
-            PlaySound(directory + sound, SND_FILENAME)
-            sleep(round(uniform(0, 3.5), 1))
+            PlaySound(destinationdirectory + sound, SND_FILENAME)
+            sleep(round(uniform(0, 2), 1))
+        print()
+
+        # Changes the pitch
+        if randint(0, 2) == 1:
+            for a in range(0, randint(1, 3)):
+                filu = choice(soundlist)
+                sound = AudioSegment.from_file(destinationdirectory + filu, format='wav')
+                octaves = round(uniform(-0.18, 0.18), 2)
+                new_sample_rate = int(sound.frame_rate * (2.0 ** octaves))
+                changedsound = sound._spawn(sound.raw_data, overrides={'frame_rate': new_sample_rate})
+                changedsound = changedsound.set_frame_rate(44100)
+                changedsound.export(destinationdirectory + filu, format='wav')
+                print(filu + ' ⭥ ' + str(octaves) + ' octaves', end="  ")
+        else:
+            print()
+
+            print()
 
         # Mutates the pattern
         for a in range(0, randint(1, 2)):
             mutationa = randint(0, len(soundlist)-1)
             mutationb = randint(0, len(soundlist)-1)
             soundlist[mutationa], soundlist[mutationb] = soundlist[mutationb], soundlist[mutationa]
-            print(soundlist[mutationa] + " <-> " + soundlist[mutationb], end="  ")
+            print(soundlist[mutationa] + " ⭤ " + soundlist[mutationb], end="  ")
 
         loop += 1
         playtime = round((time() - start) / 60, 1)
